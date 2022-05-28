5379a3ff58673cdc56d3b3f6bc8f6280117e79a1
Ayush Tripathi <ayushtripathi445@gmail.com>
2022-05-28 17:26:48 +0530

    initial setup

---
+++ contactsapp-backend/bin/main.dart
@@ -0,0 +1,12 @@
+import 'package:contactsapp_backend/contactsapp_backend.dart'
+    as contactsapp_backend;
+
+void main(List<String> arguments) {
+  // Connect and load collection
+
+  // Create server
+
+  // Create routes
+
+  // Listen for incoming connections
+}
---
+++ contactsapp-backend/test/contactsapp_backend_test.dart
@@ -0,0 +1,8 @@
+import 'package:contactsapp_backend/contactsapp_backend.dart';
+import 'package:test/test.dart';
+
+void main() {
+  // test('calculate', () {
+  //   expect(calculate(), 42);
+  // });
+}
